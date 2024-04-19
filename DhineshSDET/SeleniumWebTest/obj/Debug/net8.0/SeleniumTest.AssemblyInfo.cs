d977ace5ed2b846576dd48d939be9ba044d1b296
Dhinesh kumar Kungumaraj <dhineshkumar.kungumaraj@veradigm.com>
2024-04-19 20:41:13 +0530

    Login-Page
    Missed Files

--- DhineshSDET/SeleniumWebTest/obj/Debug/net8.0/SeleniumTest.AssemblyInfo.cs
+++ DhineshSDET/SeleniumWebTest/obj/Debug/net8.0/SeleniumTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84af3f4fe2ab93a3cfbe5d3e1ad6807c400f5e6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710c1cbc9cb60abb2d25d9d8bc84b49612e7a7a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

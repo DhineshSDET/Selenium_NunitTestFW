a66d07ccfc5f0df56061bb53ac4bb655894b6894
Dhinesh kumar Kungumaraj <dhineshkumar.kungumaraj@veradigm.com>
2024-04-26 18:22:11 +0530

    Selenium_Nunit

--- DhineshSDET/SeleniumWebTest/obj/Debug/net8.0/SeleniumTest.AssemblyInfo.cs
+++ DhineshSDET/SeleniumWebTest/obj/Debug/net8.0/SeleniumTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7480971f8356e91e48ece0fc02f570b1dc947374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca5050578821a4c04c1ad9d36b7fe6bebefe9e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DhineshSDET/WebFramework/Utilities/BaseTest.cs
+++ DhineshSDET/WebFramework/Utilities/BaseTest.cs
@@ -2,27 +2,46 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 using WebDriverManager.DriverConfigs.Impl;
-using System.Threading;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework.Interfaces;
 
 namespace WebFramework.Utilities
 {
     public class BaseTest
     {
-        //public IWebDriver driver;
+        public AventStack.ExtentReports.ExtentReports Extent = new AventStack.ExtentReports.ExtentReports();
+        public ExtentTest test;
+        String browserName;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string reportPath = projectDirectory + "//index.html";
+            var htmlReporter = new ExtentHtmlReporter(reportPath);
+            Extent.AttachReporter(htmlReporter);
+            Extent.AddSystemInfo("Host Name","Local host");
+            Extent.AddSystemInfo("Environment", "QA");
+            Extent.AddSystemInfo("Username", "DhineshKumar");
+
+        }
+
         public ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
         [SetUp]
         public void StartBrowser()
         {
+            test = Extent.CreateTest(TestContext.CurrentContext.Test.Name);
             //Configuration
-            String browserName = ConfigurationManager.AppSettings["browser"];
+            browserName = TestContext.Parameters["browserName"];
+            if (browserName == null)
+            {
+                browserName = ConfigurationManager.AppSettings["browser"];
+            }
+
             SelectBrowser(browserName);
             driver.Value.Manage().Window.Maximize();
             
@@ -61,7 +80,28 @@ namespace WebFramework.Utilities
         [TearDown]
         public void AfterTest()
         {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stackTrace = TestContext.CurrentContext.Result.StackTrace;
+            DateTime time=DateTime.Now;
+            String fileName = "Screenshot_" + time.ToString("h_mm_ss") + ".png";
+            if (status == TestStatus.Failed)
+            {
+                test.Fail("Test failed", captureScreenshot(driver.Value, fileName));
+                test.Log(Status.Fail, "test failed with logtrace" + stackTrace);
+            }
+            else if (status == TestStatus.Passed)
+            {
+                test.Pass("Test Passed");
+            }
+            Extent.Flush();
             driver.Value.Quit();
+        }
+
+        public MediaEntityModelProvider captureScreenshot(IWebDriver driver, String screenShotName)
+        {
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            var screenshot= ts.GetScreenshot().AsBase64EncodedString;
+            return MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot, screenShotName).Build();
         }
     }
 }
